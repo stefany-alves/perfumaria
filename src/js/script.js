cbd518304464099d31701890735c6f6dc98e974d
stefany <stefany.ribeiro4@aluno.ce.gov.br>
2026-04-01 12:58:55 -0300

    Update shopping cart functionality and element IDs

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,28 @@
-
 const adicionar = document.querySelectorAll(".adicionarCarrinho")
- const carrinho = document.getElementById("carrinho")
- const comprar = document.getElementById("comprarPerfumes")
+const carrinho = document.getElementById("itensCarrinho")
+const comprar = document.getElementById("comprarPerfumes")
 
- adicionar.forEach((botao) => {
+adicionar.forEach((botao) => {
     botao.addEventListener("click", (event) => {
         event.preventDefault()
 
         const produtoDiv = botao.closest(".produto")
         const tituloPerfume = produtoDiv.querySelector(".tituloPerfume").textContent
         const imgSrc = produtoDiv.querySelector("img").src
-        const img = document.createElement("img")
-        img.src = imgSrc
-        //img.alt = ""
+
         const itemCar = document.createElement("div")
+
         const checkbox = document.createElement("input")
         checkbox.type = "checkbox"
-        checkbox.classList.add
-        ("perfumeCarrinho")
-        const textoCarrinho = document.createElement("span")
-        
+        checkbox.classList.add("perfumeCarrinho")
 
-         textoCarrinho.textContent = tituloPerfume
+        const textoCarrinho = document.createElement("span")
+        textoCarrinho.textContent = tituloPerfume
         textoCarrinho.classList.add("tituloCarrinho")
+
+        const img = document.createElement("img")
+        img.src = imgSrc
+        img.width = 50
 
         itemCar.appendChild(checkbox)
         itemCar.appendChild(textoCarrinho)
@@ -30,15 +30,15 @@ const adicionar = document.querySelectorAll(".adicionarCarrinho")
 
         carrinho.appendChild(itemCar)
     })
- })
+})
 
- comprar.addEventListener("click", () => {
+comprar.addEventListener("click", () => {
     const checkBoxes = carrinho.querySelectorAll(".perfumeCarrinho")
     const titulosCarrinho = carrinho.querySelectorAll(".tituloCarrinho")
 
-    for (let i = checkBoxes.length - 1; i >= 0; i--) {
+    for (let i = 0; i < checkBoxes.length; i++) {
         if (checkBoxes[i].checked) {
-            alert("você escolheu" + titulosCarrinho[i].textContent)
+            alert("Você escolheu: " + titulosCarrinho[i].textContent)
         }
     }
- })
+})
